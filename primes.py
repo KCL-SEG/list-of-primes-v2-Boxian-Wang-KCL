307a85ecc045c100651ff32030d4ba4e8c80d9ec
Boxian Wang <101829608+Boxian-Wang-KCL@users.noreply.github.com>
2022-12-06 21:36:43 +0000

    Update primes.py
    test2

--- primes.py
+++ primes.py
@@ -6,13 +6,13 @@ def primes(number_of_primes):
     l = [2]
     n = 3
     
-    while len(list) < number_of_primes:
+    while len(l) < number_of_primes:
         isP = True
         for i in range(2, n):
             if n % i == 0:
                 isP = False
         if isP:
-            l.append(n) 
+            list.append(n) 
             
           
         n += 1       

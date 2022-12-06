31ce7cbd94eac894a214a29594d4e0bc594441ed
Boxian Wang <101829608+Boxian-Wang-KCL@users.noreply.github.com>
2022-12-06 21:33:19 +0000

    Update primes.py
    test

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    
+    l = [2]
+    n = 3
+    
+    while len(list) < number_of_primes:
+        isP = True
+        for i in range(2, n):
+            if n % i == 0:
+                isP = False
+        if isP:
+            l.append(n) 
+            
+          
+        n += 1       
+        
+    if number_of_primes <= 0:
+        raise ValueError
+    
+    return l
